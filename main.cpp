dcf0217f289aff6550d7368ae9753ed5cc43c9ee
Revager <graymane5@o2.pl>
2015-10-07 09:55:27 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "game.hpp"
+using namespace std;
+
+int main(){
+        int wybor;
+	cout << ("Project R\n");
+	cout << ("***********\n");
+	cout << ("1. Graj\n");
+	cout << ("2. Exit\n");
+cin >> wybor;
+if (wybor ==1){
+   game gra;
+
+}
+
+
+}
